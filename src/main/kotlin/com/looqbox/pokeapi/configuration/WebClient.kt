f04141627fa57d91b019732af4339d92f9623f5d
silvestr3 <fellipe.silvestre@outlook.com>
2026-05-23 21:11:20 -0300

    refactor: Move pokemon base url to application.properties

--- src/main/kotlin/com/looqbox/pokeapi/configuration/WebClient.kt
+++ src/main/kotlin/com/looqbox/pokeapi/configuration/WebClient.kt
@@ -1,15 +1,15 @@
 package com.looqbox.pokeapi.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-class WebClient {
-    private companion object {
-        const val pokeapiUrl = "https://pokeapi.co/api/v2/"
-    }
-
+class WebClient(
+    @Value($$"${pokemon.baseUrl}")
+    val pokeapiUrl: String
+) {
     @Bean
     fun webClientBuilder(): WebClient.Builder {
         return WebClient.builder()
@@ -18,7 +18,7 @@ class WebClient {
     @Bean
     fun restClient(builder: WebClient.Builder): WebClient {
         return builder
-            .baseUrl(pokeapiUrl)
+            .baseUrl(this.pokeapiUrl)
             .build()
     }
 
